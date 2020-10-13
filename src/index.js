f4c8bce8d492a920583371b1636c625185f55465
Evgenij <70840749+Greblips@users.noreply.github.com>
2020-10-13 22:13:22 +0300

    complite task ReverseInt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let str = n.toString();
+    let arr = str.split('');
+
+    if(arr[0] === '-'){
+      arr.shift();
+      return arr.reverse().join('');
+    }
+    return arr.reverse().join('');
 }
